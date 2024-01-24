4cc46389e25dc9fbc6230640bc6f5975cf4fa1cc
rpranaitis <90261967+rpranaitis@users.noreply.github.com>
2024-01-24 20:05:30 +0200

    First task done

---
+++ lesson-1/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const casual = require("casual");
+const app = express();
+const port = 3000;
+
+const randomCity = casual.city;
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+const personCredentials = `${casual.name_suffix} ${casual.first_name} ${casual.last_name}`;
+
+app.get("/", (req, res) => {
+    res.send(
+        "Random city: " +
+            randomCity +
+            "<br>" +
+            "Random number: " +
+            randomNumber +
+            "<br>" +
+            "Person: " +
+            personCredentials
+    );
+});
+
+app.listen(port, () => {
+    console.log("App is listening on the port", port);
+});
