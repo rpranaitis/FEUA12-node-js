2efbeaa93b30b8b354578151742f548f63ee7f46
rpranaitis <90261967+rpranaitis@users.noreply.github.com>
2024-01-25 18:47:29 +0200

    Joined backend with frontend and cors used

--- lesson-1/index.js
+++ lesson-1/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const casual = require("casual");
+const cors = require("cors");
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 const randomCity = casual.city;
 const randomNumber = Math.floor(Math.random() * 10) + 1;
