86e16245bdca8d401aa91cdbe07f2c2f6b436286
rpranaitis <90261967+rpranaitis@users.noreply.github.com>
2024-01-31 18:39:58 +0200

    Added eslint to the default project

--- default/index.js
+++ default/index.js
@@ -1,7 +1,7 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
 app.use(cors());
@@ -9,10 +9,10 @@ app.use(express.json());
 
 const port = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
-    res.send("OK");
+app.get('/', (req, res) => {
+  res.send('OK');
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
