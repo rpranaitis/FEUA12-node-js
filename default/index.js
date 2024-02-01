399d4fc9f33515804fc9004027d858b080de73ca
rpranaitis <90261967+rpranaitis@users.noreply.github.com>
2024-02-01 19:17:35 +0200

    Added MongoDB example in default project

--- default/index.js
+++ default/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient } = require('mongodb');
 
 require('dotenv').config();
 
@@ -8,6 +9,8 @@ app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 8080;
+const URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.o98qsjc.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`;
+const client = new MongoClient(URI);
 
 app.get('/', (req, res) => {
   res.send('OK');
